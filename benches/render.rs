d1d0d32e95b9d6d7cbd259bcc5d9fc2a388a4a49
Abby Bryant <rand0m-cloud@outlook.com>
2023-08-26 16:18:55 -0400

    add render benchmark

---
+++ benches/render.rs
@@ -0,0 +1,32 @@
+use criterion::{criterion_group, criterion_main, Criterion};
+use svg_animate::*;
+
+pub fn criterion_benchmark(c: &mut Criterion) {
+    c.bench_function("render 4k60 mp4", |b| {
+        b.iter_batched(||{}, |_| {
+            app_main(Cli {
+                input: "benches/bench.anim".into(),
+                output: "benches/out/bench_4k60.mp4".into(),
+                width: 3840,
+                height: 2160,
+                framerate: 60,
+                quiet: true
+            })
+        }, criterion::BatchSize::PerIteration)
+    });
+    c.bench_function("render 320x320, 30fps mp4", |b| {
+        b.iter_batched(||{}, |_| {
+            app_main(Cli {
+                input: "benches/bench.anim".into(),
+                output: "benches/out/bench_320_30.mp4".into(),
+                width: 320,
+                height: 320,
+                framerate: 30,
+                quiet:true
+            })
+        }, criterion::BatchSize::PerIteration)
+    });
+}
+
+criterion_group!(name = benches; config = Criterion::default().sample_size(10); targets = criterion_benchmark);
+criterion_main!(benches);
---
+++ src/lib.rs
@@ -0,0 +1,118 @@
+use std::{
+    io::Write,
+    path::PathBuf,
+    process::{Command, Stdio},
+};
+
+use analysis::Animation;
+use clap::Parser;
+use parse::Parse;
+use resvg::{
+    tiny_skia::Pixmap,
+    usvg::{self, Transform, TreeParsing},
+    Tree,
+};
+use svg::{node::element::Element, Node};
+
+mod analysis;
+mod ast;
+mod lex;
+mod parse;
+mod tokens;
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+pub struct Cli {
+    /// Path to animation script
+    pub input: PathBuf,
+
+    /// Path to output
+    pub output: PathBuf,
+
+    /// Width of the output
+    #[arg(long, default_value = "320")]
+    pub width: u32,
+
+    /// Height of the output
+    #[arg(long, default_value = "320")]
+    pub height: u32,
+
+    /// Framerate of the output
+    #[arg(long, default_value = "60")]
+    pub framerate: u32,
+
+    #[arg(short)]
+    pub quiet: bool,
+}
+
+pub fn app_main(args: Cli) {
+    let animation_source = std::fs::read_to_string(&args.input).unwrap();
+    let anim = Animation::new(
+        ast::Animation::parse_from_str(&animation_source).expect("animation didn't parse :("),
+    );
+
+    let framerate_str = args.framerate.to_string();
+    let size_str = format!("{}x{}", args.width, args.height);
+    let output_str = args.output.display().to_string();
+
+    let mut ffmpeg_args = vec![
+        "-framerate",
+        &framerate_str,
+        "-y",
+        "-f",
+        "rawvideo",
+        "-pixel_format",
+        "rgba",
+        "-video_size",
+        &size_str,
+        "-i",
+        "-",
+        &output_str,
+    ];
+
+    if args.quiet {
+        ffmpeg_args.extend(["-loglevel", "panic"]);
+    }
+
+    let mut ffmpeg = Command::new("ffmpeg")
+        .args(ffmpeg_args)
+        .stdin(Stdio::piped())
+        .spawn()
+        .expect("ffmpeg command to work");
+
+    let mut stdin = ffmpeg.stdin.take().unwrap();
+
+    let mut frame_index = 0;
+    let mut time = 0.0;
+
+    while time < anim.duration.as_secs_f32() {
+        let frame = render_frame(time, &anim, &args);
+        time += 1.0 / args.framerate as f32;
+
+        let mut image = Pixmap::new(args.width, args.height).unwrap();
+        let usvg = usvg::Tree::from_str(&frame.to_string(), &usvg::Options::default()).unwrap();
+        let svg = Tree::from_usvg(&usvg);
+        svg.render(Transform::default(), &mut image.as_mut());
+
+        frame_index += 1;
+        stdin.write_all(image.data()).unwrap();
+    }
+
+    if frame_index == 0 {
+        render_frame(0.0, &anim, &args);
+    }
+
+    drop(stdin);
+    ffmpeg.wait().unwrap();
+}
+
+pub fn render_frame(current_time: f32, animation: &Animation, args: &Cli) -> Element {
+    let frame = animation.render(current_time);
+    let mut svg = Element::new("svg");
+    svg.assign("width", args.width);
+    svg.assign("height", args.height);
+    svg.assign("xmlns", "http://www.w3.org/2000/svg");
+    svg.assign("render_time", current_time);
+    svg.append(frame);
+    svg
+}
